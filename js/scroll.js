32eb1407453fee468eada53ddafcc1b06900c79a
Muhammad Salman <meandmylearn@gmail.com>
2020-06-02 16:59:48 +0500

    Auto World layout

---
+++ js/scroll.js
@@ -0,0 +1,56 @@
+const sr = ScrollReveal();
+sr.reveal("#card1", {
+    origin: 'left',
+    distance: '50px',
+    duration: 3000
+})
+sr.reveal("#card2", {
+    origin: 'top',
+    distance: '50px',
+    duration: 2000
+})
+sr.reveal("#card3", {
+    origin: 'bottom',
+    distance: '50px',
+    duration: 3000
+})
+sr.reveal("#card4", {
+    origin: 'right',
+    distance: '50px',
+    duration: 2000
+})
+sr.reveal("#about-1", {
+    origin: 'bottom',
+    distance: '50px',
+    duration: 2000
+})
+sr.reveal("#about-2", {
+    origin: 'bottom',
+    distance: '50px',
+    duration: 3000
+})
+sr.reveal("#about-3", {
+    origin: 'bottom',
+    distance: '50px',
+    duration: 3500
+})
+sr.reveal("#fs-1", {
+    origin: 'left',
+    distance: '50px',
+    duration: 3500
+})
+sr.reveal("#fs-2", {
+    origin: 'top',
+    distance: '50px',
+    duration: 3500
+})
+sr.reveal("#fs-3", {
+    origin: 'right',
+    distance: '50px',
+    duration: 3500
+})
+sr.reveal("#fs-4", {
+    origin: 'bottom',
+    distance: '50px',
+    duration: 3500
+})
